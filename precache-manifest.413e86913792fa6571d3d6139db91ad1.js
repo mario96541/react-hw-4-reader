2c41e2d6118cc3c967b93c6e8c4328f1ba22a4c9
mario96541 <41206432+mario96541@users.noreply.github.com>
2019-08-23 13:12:32 +0300

    Updates

---
+++ precache-manifest.413e86913792fa6571d3d6139db91ad1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c399f06348d88ae4a500947dcbf4e3aa",
+    "url": "/react-hw-4-reader/index.html"
+  },
+  {
+    "revision": "74635d30575e90cd6f6f",
+    "url": "/react-hw-4-reader/static/css/main.be6363cb.chunk.css"
+  },
+  {
+    "revision": "3498cb419974eee43078",
+    "url": "/react-hw-4-reader/static/js/2.7da907a9.chunk.js"
+  },
+  {
+    "revision": "74635d30575e90cd6f6f",
+    "url": "/react-hw-4-reader/static/js/main.ac661190.chunk.js"
+  },
+  {
+    "revision": "a7a9981a82109b99c89b",
+    "url": "/react-hw-4-reader/static/js/runtime~main.7a1c459a.js"
+  }
+]);
